63982b0e96491cefe73908c079a6f7bc8d7329f3
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2024-01-17 12:22:11 -0600

    adds sessions create actiion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     user = User.new(
-      name: params[:name],
+      username: params[:username],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
