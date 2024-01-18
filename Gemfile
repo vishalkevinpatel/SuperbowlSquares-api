e55bae3effb9b54c95a6086b5867f2969c9688b3
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-01-18 12:41:45 -0500

    fixed seed files

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "jbuilder"
 gem "bcrypt", "~> 3.1.7"
 gem "jwt"
 gem "rack-cors"
+gem "seed_dump"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
-# User.create(username: "Vishal", email: "vishal@test.com", password: "password")
+# User.create(username: "Blank", email: "blank@test.com", password: "password")
 # User.create(username: "Brandon", email: "brandon@test.com", password: "password")
+# User.create(username: "Vishal", email: "vishal@test.com", password: "password")
 
 # Grid.create(code: "aaaa", user_id: 1)
 # Grid.create(code: "bbbb", user_id: 2)
-
-# Square.create(user_id: 1, grid_id: 1, x_location: 1, y_location: 1)
-# Square.create(user_id: 1, grid_id: 1, x_location: 3, y_location: 2)
-# Square.create(user_id: 2, grid_id: 1, x_location: 1, y_location: 3)
-# Square.create(user_id: 2, grid_id: 1, x_location: 4, y_location: 3)
-# Square.create(user_id: 1, grid_id: 1, x_location: 2, y_location: 2)
-# Square.create(user_id: 1, grid_id: 1, x_location: 1, y_location: 5)
