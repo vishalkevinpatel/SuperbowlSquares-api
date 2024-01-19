b2adf8d6678287c583dc25a3de9c7db776b522db
vishalkevinpatel <60484166+vishalkevinpatel@users.noreply.github.com>
2024-01-19 13:20:02 -0500

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@
 # User.create(username: "Brandon", email: "brandon@test.com", password: "password")
 # User.create(username: "Vishal", email: "vishal@test.com", password: "password")
 
-# Grid.create(user_id: 1, name: "grid 1")
-# Grid.create(user_id: 2, name: "grid 2")
+# Grid.create(user_id: 1, name: "grid 1", code: "ABCDEF", rows_digit: "0123456789", columns_digit: "0124756389")
+# Grid.create(user_id: 2, name: "grid 2", code: "GHIJKL", rows_digit: "9361842570", columns_digit: "0192756834")
